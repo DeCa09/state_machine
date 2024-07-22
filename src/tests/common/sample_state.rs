dedbd5d1edd22f5c4d416e49c3a8b12224f9d9b5
Demir Catovic <d.catovic9@gmail.com>
2024-07-23 01:44:30 +0200

    refactor: add renamed files

---
+++ src/tests/common/sample_state.rs
@@ -0,0 +1,50 @@
+use crate::state_machine::context::ContextData;
+use crate::state_machine::state::{data::StateData, State};
+
+#[derive(Debug, Clone, Default, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct SampleState {
+    input: SampleStateData,
+    output: Option<SampleStateData>,
+    context_data: SampleStateContext,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, PartialOrd, Hash, Eq, Ord)]
+
+pub struct SampleStateData;
+
+impl StateData for SampleStateData {}
+
+#[derive(Debug, Clone, Default, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct SampleStateContext;
+
+impl ContextData for SampleStateContext {}
+
+impl State for SampleState {
+    type InputData = SampleStateData;
+    type OutputData = SampleStateData;
+    type Context = SampleStateContext;
+
+    fn get_state_name(&self) -> String {
+        "Sample State".to_string()
+    }
+
+    fn get_input_data(&self) -> SampleStateData {
+        self.input.clone()
+    }
+
+    fn compute_output_data(&mut self) {
+        self.output = Some(SampleStateData::default());
+    }
+
+    fn get_output_data(&self) -> Option<SampleStateData> {
+        self.output.clone()
+    }
+
+    fn has_output_data_been_computed(&self) -> bool {
+        self.get_output_data().is_some()
+    }
+
+    fn get_context_data(&self) -> SampleStateContext {
+        self.context_data.clone()
+    }
+}
