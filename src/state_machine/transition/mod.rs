b4f47815c019e4b95f6ca24959474a2c20c88674
Demir Catovic <d.catovic9@gmail.com>
2024-07-25 21:44:37 +0200

    doc: added documentation to returned Result of transition trait method

--- src/state_machine/transition/mod.rs
+++ src/state_machine/transition/mod.rs
@@ -6,6 +6,16 @@ pub trait Transition<C: ContextData, S: State> {
 
     //type Error;
 
+
+/// Transitions the state machine to the next state.
+///
+/// # Errors
+///
+/// This function will return an error if the transition to the next state fails.
+/// Possible reasons include:
+/// - Invalid state transition due to undefined 
+/// - No conversion between data formats between states possible
+/// - ...
     fn transition_to_next_state(self) -> Result<StateMachine<C, Self::NextState>, &'static str>;
     //fn transition_to_next_state(self) -> Result<StateMachine<Self::NextState>, Self::Error>;
 }
