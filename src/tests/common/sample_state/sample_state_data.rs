126ce3b0cef6b846dd6e6fb26a3d4c845f254855
Demir Catovic <d.catovic9@gmail.com>
2024-08-02 19:57:22 +0200

    feat: add SampleStateData named field struct

--- src/tests/common/sample_state/sample_state_data.rs
+++ src/tests/common/sample_state/sample_state_data.rs
@@ -1,6 +1,15 @@
 use crate::state_machine::state::StateData;
 
 #[derive(Debug, Clone, Default, PartialEq, PartialOrd, Hash, Eq, Ord)]
-pub struct SampleStateData;
+pub struct SampleStateData{
+    state_data: String,
+}
+
+
+impl SampleStateData {
+    pub fn new(state_data: String) -> Self {
+        Self { state_data }
+    }
+}
 
 impl StateData for SampleStateData {}
