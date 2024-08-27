289e3d23d32b1336bd666cf4c19ed067595aca21
Demir Catovic <d.catovic9@gmail.com>
2024-08-27 23:52:36 +0200

    feat: implement sample state machine for testing

--- src/state_machine/mod.rs
+++ src/state_machine/mod.rs
@@ -4,9 +4,7 @@ pub mod transition;
 use crate::state_machine::state::State;
 
 pub trait StateMachine<S: State>: State {
-    fn get_current_state(&self) -> impl ToString {
-        self.get_state_name()
-    }
+    fn get_current_state(&self) -> &S;
 
     fn run(&mut self);
 
--- src/tests/common/mod.rs
+++ src/tests/common/mod.rs
@@ -1,4 +1,5 @@
 pub mod sample_state;
+pub mod sample_state_machine;
 
 pub use sample_state::{
     SampleState, SampleStateContext, SampleStateContextUpdaterBuilder, SampleStateData,
