623031e176801bfb090389d44fa3808efd21dac8
Demir Catovic <d.catovic9@gmail.com>
2024-08-27 22:57:07 +0200

    feat: define Transition trait methods

--- src/state_machine/transition/mod.rs
+++ src/state_machine/transition/mod.rs
@@ -1,6 +1,6 @@
 use crate::state_machine::{state::State, StateMachine};
 
-pub trait Transition<S: State> {
+pub trait Transition<S: State>: StateMachine<S> {
     type NextState: State;
 
     //type Error;
@@ -14,6 +14,5 @@ pub trait Transition<S: State> {
     /// - Invalid state transition due to undefined
     /// - No conversion between data formats between states possible
     /// - ...
-    fn transition_to_next_state(self) -> Result<StateMachine<Self::NextState>, &'static str>;
-    //fn transition_to_next_state(self) -> Result<StateMachine<Self::NextState>, Self::Error>;
+    fn transition_to_next_state(self) -> Result<Self::NextState, &'static str>;    //fn transition_to_next_state(self) -> Result<StateMachine<Self::NextState>, Self::Error>;
 }
