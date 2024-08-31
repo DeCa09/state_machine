8b8b9bb437c443b06712a6d16372eb680931aed9
Demir Catovic <d.catovic9@gmail.com>
2024-08-31 11:23:06 +0200

    doc: add docstring to general state trait

--- src/state_machine/state/mod.rs
+++ src/state_machine/state/mod.rs
@@ -5,6 +5,41 @@ pub mod state_data;
 
 pub use context_data::ContextData;
 pub use state_data::StateData;
+
+
+/// The `State` trait defines the behavior and characteristics of a state within a state machine.
+///
+/// This trait encompasses the key functionalities that a state must implement, including methods for
+/// retrieving state names, handling input and output data, and managing context data. It also defines
+/// the data types that are associated with the state, ensuring that all states conform to a standard
+/// interface for interacting with the rest of the state machine.
+///
+/// # Associated Types
+///
+/// - `InputData`: Represents the type of data input that the state processes. Must implement the `StateData` trait.
+/// - `OutputData`: Represents the type of data output that the state produces. Must implement the `StateData` trait.
+/// - `Context`: Represents the context or environment data associated with the state. Must implement the `ContextData` trait.
+///
+/// # Required Traits
+///
+/// Implementations of the `State` trait must also implement several Rust standard traits to ensure
+/// thread safety, comparison, and debugging capabilities:
+/// - `Debug`: Allows the state to be formatted using the `{:?}` formatter, which is useful for debugging.
+/// - `Send`, `Sync`, `Unpin`: Ensure that the state can be safely transferred and accessed across threads.
+/// - `Clone`, `PartialEq`, `PartialOrd`, `Hash`, `Eq`, `Ord`: Support comparison and hashing, which is
+///   necessary for certain data structures like sets or maps.
+///
+/// # Methods
+///
+/// The `State` trait defines several methods that must be implemented:
+///
+/// - `get_state_name`: Returns the name of the state as a string representation. Useful for identifying the current state.
+/// - `get_input_data`: Returns a reference to the input data associated with the state. This data is used for processing within the state.
+/// - `compute_output_data`: Performs computations to generate the output data from the input data. This method modifies the state to store the output data.
+/// - `get_output_data`: Returns an optional reference to the output data. If the output data has been computed, it will return `Some(&OutputData)`, otherwise `None`.
+/// - `has_output_data_been_computed`: Returns a boolean indicating whether the output data has been computed. The default implementation checks if `get_output_data` returns `Some`.
+/// - `get_context_data`: Returns a reference to the context data associated with the state. This data provides additional information or settings relevant to the state.
+
 pub trait State:
     Debug + Send + Sync + Unpin + Clone + PartialEq + PartialOrd + Hash + Eq + Ord
 {
